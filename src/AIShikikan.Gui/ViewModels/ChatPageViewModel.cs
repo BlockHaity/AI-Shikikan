689d0595d9eb5655a1f1d9e5a99baae9cb90f042
BlockHaity <1216692740@qq.com>
2026-08-08 19:37:07 +0800

    fix: 移除重复的 _selectedPersona 属性定义

--- src/AIShikikan.Gui/ViewModels/ChatPageViewModel.cs
+++ src/AIShikikan.Gui/ViewModels/ChatPageViewModel.cs
@@ -77,9 +77,6 @@ public partial class ChatPageViewModel : ViewModelBase
     private bool _rosterEnabled = true;
 
     [ObservableProperty]
-    private Persona? _selectedPersona;
-
-    [ObservableProperty]
     private IReadOnlyList<Assignment> _assignments = [];
 
     [ObservableProperty]
