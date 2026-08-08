12a9c54ef0d83abfed8898bacfa74cfcf27cc4e6
BlockHaity <1216692740@qq.com>
2026-08-08 19:10:13 +0800

    fix: 添加 System.Linq using 语句修复 ToList 编译错误

--- src/AIShikikan.Gui/ViewModels/SettingsPageViewModel.cs
+++ src/AIShikikan.Gui/ViewModels/SettingsPageViewModel.cs
@@ -1,6 +1,7 @@
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using AIShikikan.Core;
